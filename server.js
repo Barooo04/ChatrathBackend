7773c8732ec299dc7a22d1fa9719468d569c83e2
Francesco Barone <francescobarone@Mac.homenet.telecomitalia.it>
2025-01-14 11:34:39 +0100

    Prova2

--- server.js
+++ server.js
@@ -7,14 +7,22 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
-app.use(cors({
-    origin: ['http://localhost:3000', 'https://chatrathassistant.vercel.app', 'https://chatrathbackenddeployments.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+const corsOptions = {
+    origin: (origin, callback) => {
+        const allowedOrigins = ['http://localhost:3000', 'https://chatrathassistant.vercel.app'];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true  // Importante per le richieste con credentials: 'include'
-}));
-app.options('*', cors());
+    credentials: true
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(bodyParser.json());
 
 //LOGIN 
