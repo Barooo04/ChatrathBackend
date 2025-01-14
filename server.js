ebbef57f408e057f59dba3d2367657db88cc5981
Francesco Barone <francescobarone@Mac.homenet.telecomitalia.it>
2025-01-14 11:54:32 +0100

    Backend

--- server.js
+++ server.js
@@ -1,43 +1,22 @@
 const express = require('express');
-const cors = require('cors');
 const connection = require('./db');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
-const corsOptions = {
-    origin: (origin, callback) => {
-        const allowedOrigins = ['http://localhost:3000', 'https://chatrathassistant.vercel.app'];
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+app.use(bodyParser.json());
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://chatrathassistant.vercel.app', 'https://chatrathbackenddeployments.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-app.use(bodyParser.json());
-
-app.options('/api/login', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://chatrathassistant.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(200);
-});
+}));
 
 //LOGIN 
 app.post('/api/login', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://chatrathassistant.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    
     const { email, password } = req.body;
 
     connection.query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password], (err, results) => {
