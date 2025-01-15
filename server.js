cbf5295545d66b6d2eb7540fba9b90b21cc39455
Francesco Barone <francescobarone@Mac.homenet.telecomitalia.it>
2025-01-15 10:46:52 +0100

    Cors

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.sendStatus(200);
+    }
+    next();
+  });
 
 //LOGIN 
 app.post('/api/login', (req, res) => {
